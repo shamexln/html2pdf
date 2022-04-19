d215b802cddeb9df47b8376c42582524ec6b8453
shamexln <31462217+shamexln@users.noreply.github.com>
2022-04-19 15:07:00 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="app" ref="document">
+		<img width=200 src="./assets/logo.png">
+		<router-view></router-view>
+		<button @click="exportToPDF">Export to PDF</button>
+	</div>
+</template>
+
+<script>
+	import html2pdf from 'html2pdf.js'
+	
+	export default {
+    name: 'app',
+		methods: {
+			exportToPDF () {
+				html2pdf(this.$refs.document, {
+					margin: 1,
+					filename: 'document.pdf',
+					image: { type: 'jpeg', quality: 0.98 },
+					html2canvas: { dpi: 192, letterRendering: true },
+					jsPDF: { unit: 'in', format: 'letter', orientation: 'landscape' }
+				})
+			}
+		}
+  }
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
